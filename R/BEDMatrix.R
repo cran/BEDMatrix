1e10f82414d32bb29daa3df670210663556657c5
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2016-09-28 19:34:55 +0000

    version 1.3.0

--- R/BEDMatrix.R
+++ R/BEDMatrix.R
@@ -5,8 +5,12 @@ delims <- "[ \t]"
 #'
 #' \code{BEDMatrix} is an S3 class that behaves similarly to a regular
 #' \code{matrix} by implementing key methods such as \code{[}, \code{dim}, and
-#' \code{dimnames}. Subsets are extracted directly and on-demand from the binary
-#' PED file without loading the entire file into memory through memory mapping.
+#' \code{dimnames}. Subsets are extracted directly and on-demand from the
+#' binary PED file without loading the entire file into memory through memory
+#' mapping. The subsets are coded similarly to RAW files generated with the
+#' \code{--recodeA} argument in PLINK: \code{0} indicates homozygous major
+#' allele, \code{1} indicates heterozygous, and \code{2} indicates homozygous
+#' minor allele.
 #'
 #' A \code{BEDMatrix} instance can be created by providing the path to the BED
 #' file (with or without extension) as \code{path}, the number of individuals
@@ -14,14 +18,17 @@ delims <- "[ \t]"
 #' corresponds to the first six columns of a PED file) of the same name and in
 #' the same directory as the BED file exists, it is optional to provide
 #' \code{n} and the number of individuals as well as the rownames of the
-#' \code{BEDMatrix} will be detected automatically. If a BIM file (which
-#' corresponds to the MAP file that accompanies a PED file) of the same name
-#' and in the same directory as the BED file exists, it is optional to provide
-#' \code{p} and the number of markers as well as the colnames of the
-#' \code{BEDMatrix} will be detected automatically.  For very large BED file it
-#' is advised to provide \code{n} and \code{p} manually to speed up object
-#' creation. In that case \code{rownames} and \code{colnames} will be set to
-#' \code{NULL} and have to be specified manually.
+#' \code{BEDMatrix} will be detected automatically. The rownames will be
+#' generated based on the IID and FID of each individual, concatenated by
+#' \code{_}.  If a BIM file (which corresponds to the MAP file that accompanies
+#' a PED file) of the same name and in the same directory as the BED file
+#' exists, it is optional to provide \code{p} and the number of markers as well
+#' as the colnames of the \code{BEDMatrix} will be detected automatically. The
+#' colnames will be generated based on the SNP name and the minor allele,
+#' concatenated by \code{_} (similar to the colnames in a RAW file). For very
+#' large BED file it is advised to provide \code{n} and \code{p} manually to
+#' speed up object creation. In that case \code{rownames} and \code{colnames}
+#' will be set to \code{NULL} and have to be specified manually.
 #'
 #' A BED file can be created from a PED file with
 #' \href{http://pngu.mgh.harvard.edu/~purcell/plink/}{PLINK} using \code{plink
@@ -42,14 +49,15 @@ delims <- "[ \t]"
 #' @param n The number of individuals. Optional if FAM file of same name as BED
 #'   file exists. If provided, \code{rownames} will be set to \code{NULL} and
 #'   have to be provided manually.
-#' @param p The number of markers. Optional if MAP file of same name as BED file
+#' @param p The number of markers. Optional if BIM file of same name as BED file
 #'   exists. If provided, \code{colnames} will be set to \code{NULL} and have
 #'   to be provided manually.
 #' @export
+#' @example man/examples/BEDMatrix.R
 BEDMatrix <- function(path, n = NULL, p = NULL) {
     path <- path.expand(path)
     if (!file.exists(path)) {
-        # Try to add extension (common in PLINK).
+        # Try to add extension (common in PLINK)
         path <- paste0(path, ".bed")
         if (!file.exists(path)) {
             stop("File not found.")
@@ -57,38 +65,56 @@ BEDMatrix <- function(path, n = NULL, p = NULL) {
     }
     dir <- substr(path, 1, nchar(path) - 4)
     if (is.null(n)) {
-        # Check if FAM file exists.
+        # Check if FAM file exists
         if (!file.exists(paste0(dir, ".fam"))) {
             stop("FAM file of same name not found. Provide number of individuals (n).")
         } else {
             message("Extracting number of individuals and rownames from FAM file...")
-            fam <- readLines(paste0(dir, ".fam"))
-            # Determine n.
-            n <- length(fam)
-            # Determine rownames.
-            rownames <- sapply(strsplit(fam, delims), function(line) {
-                # Concatenate family ID and subject ID
-                return(paste0(line[1], "_", line[2]))
-            })
+            famPath <- paste0(dir, ".fam")
+            if (requireNamespace("data.table", quietly = TRUE)) {
+                fam <- data.table::fread(famPath, select = c(1, 2), data.table = FALSE, showProgress = FALSE)
+                # Determine n
+                n <- nrow(fam)
+                # Determine rownames
+                rownames <- paste0(fam[, 1], "_", fam[, 2])
+            } else {
+                fam <- readLines(famPath)
+                # Determine n
+                n <- length(fam)
+                # Determine rownames
+                rownames <- sapply(strsplit(fam, delims), function(line) {
+                    # Concatenate family ID and subject ID
+                    return(paste0(line[1], "_", line[2]))
+                })
+            }
         }
     } else {
         n <- as.integer(n)
         rownames <- NULL
     }
     if (is.null(p)) {
-        # Check if BIM file exists.
+        # Check if BIM file exists
         if (!file.exists(paste0(dir, ".bim"))) {
             stop("BIM file of same name not found. Provide number of markers (p).")
         } else {
             message("Extracting number of markers and colnames from BIM file...")
-            bim <- readLines(paste0(dir, ".bim"))
-            # Determine p.
-            p <- length(bim)
-            # Determine colnames.
-            colnames <- sapply(strsplit(bim, delims), function(line) {
-                # Concatenate SNP name and minor allele (like --recodeA)
-                return(paste0(line[2], "_", line[5]))
-            })
+            bimPath <- paste0(dir, ".bim")
+            if (requireNamespace("data.table", quietly = TRUE)) {
+                bim <- data.table::fread(bimPath, select = c(2, 5), data.table = FALSE, showProgress = FALSE)
+                # Determine p
+                p <- nrow(bim)
+                # Determine colnames
+                colnames <- paste0(bim[, 1], "_", bim[, 2])
+            } else {
+                bim <- readLines(bimPath)
+                # Determine p
+                p <- length(bim)
+                # Determine colnames
+                colnames <- sapply(strsplit(bim, delims), function(line) {
+                    # Concatenate SNP name and minor allele (like --recodeA)
+                    return(paste0(line[2], "_", line[5]))
+                })
+            }
         }
     } else {
         p <- as.integer(p)
@@ -111,7 +137,12 @@ print.BEDMatrix <- function(x, ...) {
     dims <- dim(x)
     n <- dims[1]
     p <- dims[2]
-    cat(paste(n, "x", p, "BEDMatrix"), "\n")
+    cat("BEDMatrix: ", n, " x ", p, " [", attr(x, "path"), "]\n", sep = "")
+}
+
+#' @export
+str.BEDMatrix <- function(object, ...) {
+    print(object)
 }
 
 #' @export
@@ -127,21 +158,17 @@ print.BEDMatrix <- function(x, ...) {
         } else if (class(i) == "logical") {
             i <- which(rep_len(i, n))
         } else if (class(i) == "character") {
-            i <- sapply(i, function(name) {
-                which(rownames(x) == name)
-            }, USE.NAMES = FALSE)
+            i <- match(i, rownames(x))
         }
         if (missing(j)) {
             j <- 1:p
         } else if (class(j) == "logical") {
             j <- which(rep_len(j, p))
         } else if (class(j) == "character") {
-            j <- sapply(j, function(name) {
-                which(colnames(x) == name)
-            }, USE.NAMES = FALSE)
+            j <- match(j, colnames(x))
         }
         subset <- rcpp_obj$matrixSubset(x, i, j)
-        # Let R handle drop behavior.
+        # Let R handle drop behavior
         if (drop == TRUE && (nrow(subset) == 1 || ncol(subset) == 1)) {
             subset <- subset[, ]
         }
--- README.md
+++ README.md
@@ -2,15 +2,17 @@ BEDMatrix
 =========
 
 [![Travis-CI Build Status](https://travis-ci.org/QuantGen/BEDMatrix.svg?branch=master)](https://travis-ci.org/QuantGen/BEDMatrix)
-[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/BEDMatrix)](http://cran.r-project.org/package=BEDMatrix)
+[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/BEDMatrix)](https://cran.r-project.org/package=BEDMatrix)
 
 BEDMatrix is an R package that provides a wrapper around [binary PED (also known as BED) files](http://pngu.mgh.harvard.edu/~purcell/plink/data.shtml#bed), one of the genotype/phenotype file formats of [PLINK](http://pngu.mgh.harvard.edu/~purcell/plink/), the whole genome association analysis toolset. BEDMatrix objects are created in R by simply providing the path to a BED file and once created, they behave similarly to regular matrices with the advantage that genotypes are retrieved on demand without loading the entire file into memory. This allows handling of very large files with limited use of memory. Technically, a BEDMatrix is a memory-mapped matrix backed by a binary PED file.
+
+This package is deliberately kept simple. For computational methods that use BEDMatrix check out the [BGData package](https://github.com/QuantGen/BGData).
 
 
 Example
 -------
 
-This example uses a very simple BED file that is bundled with the R package. It was generated from the PLINK files in the [`inst/extdata` folder](https://github.com/QuantGen/BEDMatrix/tree/master/inst/extdata) ([see below on how to convert a PED file to a BED file](#how-do-i-create-a-bed-file-from-a-ped-file-using-plink)).
+This example uses a very simple BED file that is bundled with this R package. It was generated from the PLINK files in the [`inst/extdata` folder](https://github.com/QuantGen/BEDMatrix/tree/master/inst/extdata) using `plink --file example --make-bed --out example`.
 
 To get the path to the example BED file (`system.file` finds the full file names of files in packages and is only used to find the example data):
 
@@ -26,24 +28,21 @@ Extracting number of individuals and rownames from FAM file...
 Extracting number of markers and colnames from BIM file...
 ```
 
-To print the dimensions of the BEDMatrix object:
+To get the dimensions of the BEDMatrix object:
 
 ```r
 > dim(m)
 [1] 6 3
 ```
 
-To print the entire BEDMatrix object:
+To extract a subset of the BEDMatrix object:
 
 ```r
-> m[]
+> m[1:3, ]
     snp1_G snp2_1 snp3_A
 1_1      2      0      0
 1_2      0     NA      1
 1_3     NA      1      1
-2_1      0      0     NA
-2_2      0      0     NA
-2_3      0      0      2
 ```
 
 
@@ -62,3 +61,9 @@ To get the current development version from GitHub:
 # install.packages("devtools")
 devtools::install_github("QuantGen/BEDMatrix")
 ```
+
+
+Documentation
+-------------
+
+Further documentation can be found on [RDocumentation](http://www.rdocumentation.org/packages/BEDMatrix).
